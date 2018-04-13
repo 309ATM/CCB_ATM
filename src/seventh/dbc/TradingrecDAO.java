e42a1ac9f2b24354f23646c553d94c956d7297cd
JachinYin <1106025235@qq.com>
2018-04-13 21:04:34 +0800

    改正数据库的记录查询
    不需要在前台处理日期，只需要输入选择的日期，数据库部分完成结果的查询

--- src/seventh/dbc/TradingrecDAO.java
+++ src/seventh/dbc/TradingrecDAO.java
@@ -1,13 +1,15 @@
 package seventh.dbc;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class TradingrecDAO extends DAO<Tradingrec>{
+public class TradingrecDAO extends DAO<Tradingrec> {
 
-	/** 
+	/**
 	 * 插入一条交易记录
+	 * 
 	 * @param cardnum
 	 * @param tradeDate
 	 * @param tradeMoney
@@ -16,68 +18,67 @@ public class TradingrecDAO extends DAO<Tradingrec>{
 	 * @param fee
 	 * @return
 	 */
-	public void insertRecording(long card, float money, String tradeType, long tradeTarget, float fee){
+	public void insertRecording(long card, float money, String tradeType, long tradeTarget, float fee) {
 		String sql = "insert into tradingrec(cardnum,tradeDate,tradeMoney,tradeType,tradeTarget,fee) values (?,?,?,?,?,?)";
 		// 自动获取当前日期
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
 		String date = sdf.format(new Date());
-		update(sql, card,date,money,tradeType,tradeTarget, fee);
+		update(sql, card, date, money, tradeType, tradeTarget, fee);
 	}
-	
-	
+
 	/**
 	 * 获取存款限额
+	 * 
 	 * @param card
 	 * @return
 	 */
-	public float getDepositLimit(long card){
+	public float getDepositLimit(long card) {
 		// 存款限额为 4 万元
 		float depositLimit = 40000;
 		// 记录当天已经存款的金额
 		float depositNum = 0;
-		String sql = "select * from tradingrec where cardnum = ? "
-				+ " and tradeType = ? and tradeDate like ?";
+		String sql = "select * from tradingrec where cardnum = ? " + " and tradeType = ? and tradeDate like ?";
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String tradedate = sdf.format(new Date());
-		List<Tradingrec> tradingrecs = getForList(sql, card,"存款","%"+tradedate+"%");
+		List<Tradingrec> tradingrecs = getForList(sql, card, "存款", "%" + tradedate + "%");
 		// 计算当天该卡号的所有存款金额
-		for(Tradingrec t:tradingrecs) {
+		for (Tradingrec t : tradingrecs) {
 			depositNum += t.getTradeMoney();
 		}
 		return depositLimit - depositNum;
 	}
-	
-	//获取取款限额
-	public float getWithdrawalsLimit(long card){
-		
+
+	// 获取取款限额
+	public float getWithdrawalsLimit(long card) {
+
 		float withdrawalsLimit = 20000;
 		float withdrawalsNum = 0;
-		String sql = "select * from tradingrec where cardnum = ? "
-				+ " and tradeType = ? and tradeDate like ?";
+		String sql = "select * from tradingrec where cardnum = ? " + " and tradeType = ? and tradeDate like ?";
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		String tradedate = sdf.format(new Date());
-		List<Tradingrec> tradingrecs = getForList(sql, card,"取款","%"+tradedate+"%");
-		
+		List<Tradingrec> tradingrecs = getForList(sql, card, "取款", "%" + tradedate + "%");
+
 		// 计算当天该卡号的所有取款金额
-		for(Tradingrec t:tradingrecs) {
+		for (Tradingrec t : tradingrecs) {
 			withdrawalsNum += t.getTradeMoney();
 		}
 		return withdrawalsLimit - withdrawalsNum;
 	}
-	
-	
+
 	/**
 	 * 获取指定时间段内的交易记录
+	 * 
 	 * @param cardNumber
 	 * @param date
-	 * @return 
+	 * @return
 	 */
-	public String[][] getSpecifiedRecording(long card,String[] date){
+	public String[][] getSpecifiedRecording(long card, String[] date) {
+
 		String sql = "select * from tradingrec where cardnum = ? "
-				+ " and tradeDate > ? and tradeDate < ?";
-		List<Tradingrec> tradingrecs = getForList(sql, card,date[0],date[1]);
+				+ " and tradeDate > ? and tradeDate < ? order by tradeDate desc";
+		List<Tradingrec> tradingrecs = getForList(sql, card, SQLDate(date[0]), SQLDate(date[1]));
 		String[][] records = new String[tradingrecs.size()][7];
-		for (int i=0;i<tradingrecs.size();i++) {
+		for (int i = 0; i < tradingrecs.size(); i++) {
 			records[i][0] = String.valueOf(tradingrecs.get(i).getCardNum());
 			records[i][1] = String.valueOf(tradingrecs.get(i).getTradeDate());
 			records[i][2] = String.valueOf(tradingrecs.get(i).getTradeMoney());
@@ -86,5 +87,21 @@ public class TradingrecDAO extends DAO<Tradingrec>{
 			records[i][5] = String.valueOf(tradingrecs.get(i).getFee());
 		}
 		return records;
-	}	
+	}
+
+	//将获取记录的日期自动加一天
+	private static String SQLDate(String source) {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		try {
+			date = simpleDateFormat.parse(source);
+		} catch (Exception e) {
+			System.out.println("fas");
+		}
+		Calendar calendar = Calendar.getInstance(); // 得到日历
+		calendar.setTime(date);// 把当前时间赋给日历
+		calendar.add(Calendar.DATE, 1);
+		date = calendar.getTime();
+		return simpleDateFormat.format(date);
+	}
 }
