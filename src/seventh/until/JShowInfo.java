0701c54cbae9774cf55d6e4c80fbd2d71c70a88c
JachinYin <1106025235@qq.com>
2018-04-16 23:54:46 +0800

    为信息显示对话框添加用户修改信息部分的界面及功能

--- src/seventh/until/JShowInfo.java
+++ src/seventh/until/JShowInfo.java
@@ -9,6 +9,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
@@ -23,7 +24,7 @@ import javax.swing.border.LineBorder;
  */
 @SuppressWarnings("serial")
 public final class JShowInfo extends JDialog {
-	
+
 	public JFrame frame;
 	private JTextField text_name;
 	private JTextField text_sex;
@@ -42,9 +43,11 @@ public final class JShowInfo extends JDialog {
 	private JLabel label_idcard;
 	private JLabel label_phone;
 	private JLabel label_address1;
-	private JLabel label_bg; 
+	private JLabel label_bg;
 
-	public boolean isConfirm;	//判断用户按下取消按钮还是确定按钮
+	public boolean isConfirm; // 判断用户按下取消按钮还是确定按钮
+	public String[] info;
+	private JButton btn_change;
 
 	public static void main(String[] args) {
 		try {
@@ -70,7 +73,7 @@ public final class JShowInfo extends JDialog {
 		this.dispose();
 	}
 
-	public void setBtnText(String text){
+	public void setBtnText(String text) {
 		btn_confirm.setText(text);
 	}
 
@@ -100,73 +103,73 @@ public final class JShowInfo extends JDialog {
 		label_name = new JLabel("名字");
 		label_name.setFont(new Font("幼圆", Font.PLAIN, 20));
 		label_name.setBounds(100, 173, 81, 30);
-		
+
 		label_type = new JLabel("账户类型");
 		label_type.setFont(new Font("幼圆", Font.PLAIN, 18));
 		label_type.setBounds(372, 174, 79, 30);
-		
+
 		label_sex = new JLabel("性别");
 		label_sex.setFont(new Font("幼圆", Font.PLAIN, 18));
 		label_sex.setBounds(100, 241, 81, 30);
-		
+
 		label_Status = new JLabel("账户状态");
 		label_Status.setFont(new Font("幼圆", Font.PLAIN, 18));
 		label_Status.setBounds(372, 241, 79, 30);
-		
+
 		label_idcard = new JLabel("身份证号");
 		label_idcard.setFont(new Font("幼圆", Font.PLAIN, 18));
 		label_idcard.setBounds(100, 304, 81, 30);
-		
+
 		label_phone = new JLabel("手机号码");
 		label_phone.setFont(new Font("幼圆", Font.PLAIN, 18));
 		label_phone.setBounds(100, 367, 81, 30);
-		
+
 		label_address1 = new JLabel("家庭住址");
 		label_address1.setFont(new Font("幼圆", Font.PLAIN, 18));
 		label_address1.setBounds(100, 434, 81, 30);
-		
+
 		text_name = new JTextField();
 		text_name.setBackground(Color.WHITE);
 		text_name.setEditable(false);
 		text_name.setFont(new Font("幼圆", Font.PLAIN, 20));
 		text_name.setBounds(185, 174, 140, 30);
 		text_name.setColumns(10);
-		
+
 		text_type = new JTextField();
 		text_type.setBackground(Color.WHITE);
 		text_type.setEditable(false);
 		text_type.setFont(new Font("幼圆", Font.PLAIN, 20));
 		text_type.setColumns(10);
 		text_type.setBounds(465, 174, 140, 30);
-		
+
 		text_sex = new JTextField();
 		text_sex.setBackground(Color.WHITE);
 		text_sex.setEditable(false);
 		text_sex.setFont(new Font("幼圆", Font.PLAIN, 20));
 		text_sex.setColumns(10);
 		text_sex.setBounds(185, 241, 140, 30);
-		
+
 		text_status = new JTextField();
 		text_status.setBackground(Color.WHITE);
 		text_status.setEditable(false);
 		text_status.setFont(new Font("幼圆", Font.PLAIN, 20));
 		text_status.setColumns(10);
 		text_status.setBounds(465, 241, 140, 30);
-		
+
 		text_idcard = new JTextField();
 		text_idcard.setBackground(Color.WHITE);
 		text_idcard.setEditable(false);
 		text_idcard.setFont(new Font("幼圆", Font.PLAIN, 20));
 		text_idcard.setColumns(10);
 		text_idcard.setBounds(185, 304, 420, 30);
-		
+
 		text_phone = new JTextField();
 		text_phone.setBackground(Color.WHITE);
 		text_phone.setEditable(false);
 		text_phone.setFont(new Font("幼圆", Font.PLAIN, 20));
 		text_phone.setColumns(10);
 		text_phone.setBounds(185, 367, 413, 30);
-		
+
 		text_address_1 = new JTextArea();
 		text_address_1.setFont(new Font("幼圆", Font.PLAIN, 20));
 		text_address_1.setEditable(false);
@@ -174,13 +177,16 @@ public final class JShowInfo extends JDialog {
 		text_address_1.setLineWrap(true);
 		text_address_1.setBounds(185, 434, 413, 64);
 		text_address_1.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		
+
 		btn_confirm = new JButton("确认");
 		btn_confirm.setBounds(212, 527, 113, 42);
-		
+
 		btn_cancel = new JButton("取消");
 		btn_cancel.setBounds(372, 527, 113, 42);
-		
+
+		btn_change = new JButton("确认修改");
+		btn_change.setBounds(212, 527, 113, 42);
+
 		label_bg = new JLabel("");
 		label_bg.setIcon(new ImageIcon("E:\\Code\\java\\CCB_ATM\\img\\UserInformation.png"));
 		label_bg.setBounds(0, 0, 707, 603);
@@ -200,7 +206,6 @@ public final class JShowInfo extends JDialog {
 		north.add(label_sex);
 		north.add(label_Status);
 		north.add(label_type);
-		
 
 		north.add(text_address_1);
 		north.add(text_idcard);
@@ -209,20 +214,21 @@ public final class JShowInfo extends JDialog {
 		north.add(text_sex);
 		north.add(text_status);
 		north.add(text_type);
-		
-		north.add(btn_cancel);
-		north.add(btn_confirm);
-		
-		north.add(label_bg);
-		
-		getContentPane().add(north, null);
 
+		north.add(btn_cancel);
+		north.add(btn_change);
+		north.add(btn_confirm);
+
+		north.add(label_bg);
+
+		getContentPane().add(north, null);
+		btn_change.setVisible(false);
 	}
 
 	/**
-	 * 添加组件数据
+	 * 挂失解挂冻结解冻添加用户信息
 	 */
-	public void addComponentData(String[] info){
+	public void StatusOpera(String[] info) {
 		text_name.setText(info[0]);
 		text_type.setText(info[1]);
 		text_sex.setText(info[2]);
@@ -231,49 +237,81 @@ public final class JShowInfo extends JDialog {
 		text_phone.setText(info[5]);
 		text_address_1.setText(info[6]);
 	}
-	
+
 	/**
-	 * 添加组件数据2
+	 * 用户查看信息
 	 */
-	public void addComponentData2(String[] info){
+	public void QueryUserInfo(String[] info) {
 		text_name.setText(info[0]);
 		text_sex.setText(info[1]);
 		text_idcard.setText(info[2]);
 		text_phone.setText(info[3]);
 		text_address_1.setText(info[4]);
-		
+
 		label_type.setVisible(false);
 		label_Status.setVisible(false);
-		
+
 		text_status.setVisible(false);
 		text_type.setVisible(false);
 	}
-	
+
 	/**
 	 * 用户修改信息
 	 */
-	public void changeInfo(String[] info){
+	public void ChangeUserInfo(String[] info) {
 		text_name.setText(info[0]);
 		text_sex.setText(info[1]);
 		text_idcard.setText(info[2]);
 		text_phone.setText(info[3]);
 		text_address_1.setText(info[4]);
-		
+
 		label_type.setVisible(false);
 		label_Status.setVisible(false);
-		
+
 		text_status.setVisible(false);
 		text_type.setVisible(false);
-		
+
 		text_name.setEditable(true);
 		text_sex.setEditable(true);
 		text_idcard.setEditable(true);
 		text_phone.setEditable(true);
 		text_address_1.setEditable(true);
-		
-		
+
+		btn_change.setVisible(true);
+
 	}
-	
+
+	public void ConfirmChange() {
+		String tips = "<html>输入信息有误<br>";
+		info = new String[6];// 接收新信息
+		info[0] = "sucess";
+		info[1] = text_name.getText();
+		info[2] = text_sex.getText();
+		info[3] = text_idcard.getText();
+		info[4] = text_phone.getText();
+		info[5] = text_address_1.getText();
+		if (info[5].isEmpty() || info[1].isEmpty() || info[2].isEmpty() || info[3].isEmpty() || info[4].isEmpty()) {
+			JOptionPane.showMessageDialog(frame, "请输入信息");
+			info[0] = "faild";
+		} else {
+			if (!info[2].equals("男") && !info[2].equals("女")) {
+				tips += "请输入正确性别<br>";
+				info[0] = "fail";
+			}
+			if (info[3].length() != 18) {
+				tips += "身份证号长度不对<br>";
+				info[0] = "fail";
+			}
+			if (info[4].length() != 11) {
+				tips += "手机号码长度不对<br>";
+				info[0] = "fail";
+			}
+		}
+		if (info[0].equals("fail")) {
+			JOptionPane.showMessageDialog(frame, tips);
+		}
+	}
+
 	/**
 	 * 添加监听
 	 */
@@ -281,6 +319,7 @@ public final class JShowInfo extends JDialog {
 		ButtonActionListener buttonActionListener = new ButtonActionListener();
 		btn_cancel.addActionListener(buttonActionListener);
 		btn_confirm.addActionListener(buttonActionListener);
+		btn_change.addActionListener(buttonActionListener);
 
 	}
 
@@ -288,7 +327,7 @@ public final class JShowInfo extends JDialog {
 	 * 设置对话框属性
 	 */
 	private void setDialogAttribute() {
-		
+
 		this.setTitle("账户信息");
 		this.setBounds(680, 210, 711, 641);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
@@ -304,14 +343,22 @@ public final class JShowInfo extends JDialog {
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getSource() == btn_confirm){
+			if (e.getSource() == btn_confirm) {
 				isConfirm = true;
 				closeJSM();
 			}
-			if(e.getSource() == btn_cancel){
+			if (e.getSource() == btn_cancel) {
 				isConfirm = false;
 				closeJSM();
 			}
+			if (e.getSource() == btn_change) {
+				ConfirmChange();
+				if(info[0].equals("sucess")){
+					isConfirm = true;
+					closeJSM();
+				}
+			}
 		}
 	}
+
 }
